964d0ab18b0cf3603c9c41b5a23a7d4cb8ced56f
Rachmad Nur Hayat <rachmadnurhayat@gmail.com>
2021-10-08 14:16:35 +0700

    Preparing Layout

--- application/views/schedule/schedule_wrapper.php
+++ application/views/schedule/schedule_wrapper.php
@@ -12,8 +12,74 @@
             </div>
             </div>
         <div class="panel-body" id="panel-body">
+            <div data-scrollbar="true" data-height="280px">
+                <div class="row">
+                    <div class="col-4">
+                        <div class="card text-center border-0">
+                            <div class="card-header fw-bold">
+                                READY
+                            </div>
+                            <div class="card-body">
+                                <div class="card border-0 mb-2" style="text-align: left;">
+                                    <div class="card-body">
+                                        <h4 class="card-title">Special title treatment</h4>
+                                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                    </div>
+                                    <div class="card-footer fw-bold">
+                                        2 days ago
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="card text-center border-0">
+                            <div class="card-header fw-bold">
+                                ON GOING
+                            </div>
+                            <div class="card-body">
+                                <div class="card border-0 mb-2" style="text-align: left;">
+                                    <div class="card-body">
+                                        <h4 class="card-title">Special title treatment</h4>
+                                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                    </div>
+                                    <div class="card-footer fw-bold">
+                                        2 days ago
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-            
+                    <div class="col-4">
+                        <div class="card text-center border-0">
+                            <div class="card-header fw-bold">
+                                READY
+                            </div>
+                            <div class="card-body">
+                                <div class="card border-0 mb-2" style="text-align: left;">
+                                    <div class="card-body">
+                                        <h4 class="card-title">Special title treatment</h4>
+                                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                    </div>
+                                    <div class="card-footer fw-bold">
+                                        2 days ago
+                                    </div>
+                                </div>
+                                <div class="card border-0 mb-2" style="text-align: left;">
+                                    <div class="card-body">
+                                        <h4 class="card-title">Special title treatment</h4>
+                                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                    </div>
+                                    <div class="card-footer fw-bold">
+                                        2 days ago
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 		</div>
 	</div>
 </div>
